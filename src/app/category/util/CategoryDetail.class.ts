ffca8805d3e43c1d8770d4deb716a8f16a60b9e6
Gabriel Barros <gabrielbarros201612@gmail.com>
2022-01-05 09:46:59 -0300

    Adicionando imagens as categorias

--- src/app/category/util/CategoryDetail.class.ts
+++ src/app/category/util/CategoryDetail.class.ts
@@ -2,10 +2,12 @@ export class CategoryDetail {
     id: number;
     name: string;
     tag: string;
+    background: string;
 
-    constructor(id:number,name: string, tag: string) {
+    constructor(id: number, name: string, tag: string, background: string) {
         this.id = id;
         this.name = name;
         this.tag = tag;
+        this.background = background;
     }
 }
